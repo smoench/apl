75e4c43df9dd88179662ffa62f0d5b3726b146eb
Simon Mönch <simonmoench@gmail.com>
2014-07-16 08:17:39 +0200

    added security checks

---
+++ src/APL/Security/Policy.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * Copyright 2014 Simon Mönch.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace APL\Security;
+
+use APL\Command;
+
+interface Policy
+{
+    public function check(SecurityContextInterface $context, Command $command);
+}
